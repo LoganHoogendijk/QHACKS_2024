cb0852b65875079526f22bc96d9297c9b6b24f9b
Samuel Emard-Thibault <sam5thibault@gmail.com>
2024-02-03 15:04:02 -0500

    models added

---
+++ backend/qhacks/main/models.py
@@ -0,0 +1,44 @@
+import uuid
+from django.db import models
+from storages.backends.s3boto3 import S3Boto3Storage
+from django.contrib.auth.models import User
+from django.conf import settings
+
+# Custom storage backend for S3
+class S3Storage(S3Boto3Storage):
+    location = settings.AWS_STORAGE_BUCKET_NAME
+
+# determine the upload path
+def leaf_image_upload_path(instance, filename):
+    # use the crop's id (UUID) as the subdirectory
+    """
+    S3 dir looks like this:
+    s3-name/
+        |   |-- 1234-5678 (crop id)/
+        |   |   |-- leaf1.jpg
+                |-- leaf2.jpg
+        |   |-- ...
+    """
+    crop_id = instance.crops.first().id
+    return f'person/{crop_id}/{filename}'
+
+class UserProfile(models.Model):
+    # has username and password from the user model
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    id = models.UUIDField(default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    crops = models.ManyToManyField('Crop', related_name='users')
+
+class Crop(models.Model):
+    label = models.CharField(max_length=100)
+    leaves = models.ManyToManyField('Leaf', related_name='crops')
+    crop_type = models.CharField(max_length=100)
+
+class Leaf(models.Model):
+    image = models.ImageField(upload_to=leaf_image_upload_path, storage=S3Storage(), null=True, blank=True)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    recommendations = models.ManyToManyField('Recommendation', related_name='leaves')
+
+class Recommendation(models.Model):
+    content = models.CharField(max_length=500)
+    complete = models.BooleanField(default=False)
--- backend/qhacks/qhacks/settings.py
+++ backend/qhacks/qhacks/settings.py
@@ -32,6 +32,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = [os.environ['ALLOWED_HOSTS']]
 
+# settings.py
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY =os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_S3_BUCKET_NAME']
+AWS_S3_REGION_NAME = 'ca-central-1'
+
+# Use Amazon S3 for storage for uploaded media files.
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Use Amazon S3 for static files storage.
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Application definition
 
@@ -42,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'main',]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
